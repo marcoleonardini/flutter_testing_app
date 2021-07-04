74440821d0f7621c0b22c31baaa65882cb218e84
Marco Leonardini <marco_leonardini@hotmail.com>
2021-07-03 20:51:47 -0400

    Added Service Repository Tests

---
+++ test/repositories/character_repository_test.dart
@@ -0,0 +1,116 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_testing_app/repositories/character_repository.dart';
+import 'package:flutter_testing_app/services/models/character_models.dart';
+import 'package:flutter_testing_app/services/remote/character_remote_service.dart';
+import 'package:mocktail/mocktail.dart';
+
+class CharacterRemoteServiceMock extends Mock
+    implements CharacterRemoteService {}
+
+void main() {
+  group('Testing Characters Remote ', () {
+    final characterRemoteServiceMock = CharacterRemoteServiceMock();
+    final characterRepositoryImpl = CharacterRepositoryImpl(
+      characterRemoteService: characterRemoteServiceMock,
+    );
+    test('- Success', () async {
+      /// Given
+      // final characterRepositoryImplMock
+
+      /// When
+      when(() => characterRemoteServiceMock.getAllCharacters()).thenAnswer(
+        (_) => Future.value(
+          [
+            const CharacterModel(
+              charId: 1,
+              name: "Walter White",
+              birthday: "09-07-1958",
+              occupation: ["High School Chemistry Teacher", "Meth King Pin"],
+              img:
+                  "https://images.amcnetworks.com/amc.com/wp-content/uploads/2015/04/cast_bb_700x1000_walter-white-lg.jpg",
+              status: "Deceased",
+              appearance: [1, 2, 3, 4, 5],
+              nickname: "Heisenberg",
+              portrayed: "Bryan Cranston",
+              betterCallSaulAppearance: [],
+              category: '',
+            )
+          ],
+        ),
+      );
+
+      final res = await characterRepositoryImpl.getAllCharacters();
+
+      /// Then
+      expect(res, isA<CharactersResponse>());
+      expect(res.results, isNotNull);
+      expect(res.error, isNull);
+    });
+    test('- Error Captured', () async {
+      /// Given
+      // final characterRepositoryImplMock
+
+      /// When
+      when(() => characterRemoteServiceMock.getAllCharacters())
+          .thenThrow(Exception('oops'));
+
+      final res = await characterRepositoryImpl.getAllCharacters();
+
+      /// Then
+      expect(res, isA<CharactersResponse>());
+      expect(res.results, isNull);
+      expect(res.error, isNotNull);
+    });
+  });
+
+  group('Testing Character Remote ', () {
+    final characterRemoteServiceMock = CharacterRemoteServiceMock();
+    final characterRepositoryImpl = CharacterRepositoryImpl(
+      characterRemoteService: characterRemoteServiceMock,
+    );
+    test('- Success', () async {
+      /// Given
+      // final characterRepositoryImplMock
+
+      /// When
+      when(() => characterRemoteServiceMock.characterById(1)).thenAnswer(
+        (_) => Future.value(const CharacterModel(
+          charId: 1,
+          name: "Walter White",
+          birthday: "09-07-1958",
+          occupation: ["High School Chemistry Teacher", "Meth King Pin"],
+          img:
+              "https://images.amcnetworks.com/amc.com/wp-content/uploads/2015/04/cast_bb_700x1000_walter-white-lg.jpg",
+          status: "Deceased",
+          appearance: [1, 2, 3, 4, 5],
+          nickname: "Heisenberg",
+          portrayed: "Bryan Cranston",
+          betterCallSaulAppearance: [],
+          category: '',
+        )),
+      );
+
+      final res = await characterRepositoryImpl.characterById(1);
+
+      /// Then
+      expect(res, isA<CharacterResponse>());
+      expect(res.results, isNotNull);
+      expect(res.error, isNull);
+    });
+    test('- Error Captured', () async {
+      /// Given
+      // final characterRepositoryImplMock
+
+      /// When
+      when(() => characterRemoteServiceMock.characterById(1))
+          .thenThrow(Exception('oops'));
+
+      final res = await characterRepositoryImpl.characterById(1);
+
+      /// Then
+      expect(res, isA<CharacterResponse>());
+      expect(res.results, isNull);
+      expect(res.error, isNotNull);
+    });
+  });
+}
--- test/services/remote/character_remote_service_test.dart
+++ test/services/remote/character_remote_service_test.dart
@@ -1,57 +1,214 @@
+import 'package:dio/dio.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_testing_app/config/endpotins.dart';
+
 import 'package:flutter_testing_app/services/models/character_models.dart';
 import 'package:flutter_testing_app/services/remote/character_remote_service.dart';
+
 import 'package:mocktail/mocktail.dart';
 
-class CharacterRemoteServiceMock extends Mock
-    implements CharacterRemoteService {}
+class DioMock extends Mock implements Dio {}
 
 void main() {
-  group('Testing Character Remote ', () {
-    final characterRemoteServiceMock = CharacterRemoteServiceMock();
+  group('Testing Characters Remote Get All ', () {
+    final dioMock = DioMock();
+    final characterRemoteService = CharacterRemoteService(dio: dioMock);
+    setUpAll(() => reset(dioMock));
     test('- Success', () async {
       /// Given
-      // final characterRemoteServiceMock
+      // final characterRemoteService
 
       /// When
-      when(() => characterRemoteServiceMock.getAllCharacters()).thenAnswer(
-        (_) => Future.value(
-          [
-            const CharacterModel(
-              charId: 1,
-              name: "Walter White",
-              birthday: "09-07-1958",
-              occupation: ["High School Chemistry Teacher", "Meth King Pin"],
-              img:
-                  "https://images.amcnetworks.com/amc.com/wp-content/uploads/2015/04/cast_bb_700x1000_walter-white-lg.jpg",
-              status: "Deceased",
-              appearance: [1, 2, 3, 4, 5],
-              nickname: "Heisenberg",
-              portrayed: "Bryan Cranston",
-              betterCallSaulAppearance: [],
-              category: '',
-            )
-          ],
+      when(() => dioMock.get<List<Map<String, dynamic>>>(
+            AppEndpoints.allCharacters,
+          )).thenAnswer(
+        (_) async => Response<List<Map<String, dynamic>>>(
+          data: [],
+          statusCode: 200,
+          requestOptions: RequestOptions(path: ''),
         ),
       );
 
-      final res = await characterRemoteServiceMock.getAllCharacters();
+      final res = await characterRemoteService.getAllCharacters();
 
       /// Then
       expect(res, isA<List<CharacterModel>>());
+
+      verify(() => dioMock.get(AppEndpoints.allCharacters)).called(1);
+      verifyNoMoreInteractions(dioMock);
     });
     test('- Exception', () async {
       /// Given
       // final characterRemoteServiceMock
 
       /// When
-      when(() => characterRemoteServiceMock.getAllCharacters())
-          .thenThrow(Exception('oops'));
+      when(() => dioMock.get<List<Map<String, dynamic>>>(
+          AppEndpoints.allCharacters)).thenThrow(Exception());
       try {
-        final res = await characterRemoteServiceMock.getAllCharacters();
+        await characterRemoteService.getAllCharacters();
       } catch (e) {
         /// Then
         expect(e, isA<Exception>());
+        verify(() => dioMock.get(AppEndpoints.allCharacters)).called(1);
+        verifyNoMoreInteractions(dioMock);
+      }
+    });
+    test('- No 200', () async {
+      /// Given
+      // final characterRemoteServiceMock
+
+      /// When
+      when(() => dioMock.get<List<Map<String, dynamic>>>(
+            AppEndpoints.allCharacters,
+          )).thenAnswer(
+        (_) async => Response<List<Map<String, dynamic>>>(
+          data: [],
+          statusCode: 0,
+          requestOptions: RequestOptions(path: ''),
+        ),
+      );
+      try {
+        await characterRemoteService.getAllCharacters();
+      } catch (e) {
+        /// Then
+        expect(e, isA<Exception>());
+        verify(() => dioMock.get(AppEndpoints.allCharacters)).called(1);
+        verifyNoMoreInteractions(dioMock);
+      }
+    });
+    test('- null data', () async {
+      /// Given
+      // final characterRemoteServiceMock
+
+      /// When
+      when(() => dioMock.get<List<Map<String, dynamic>>>(
+          AppEndpoints.allCharacters)).thenAnswer(
+        (_) async => Response<List<Map<String, dynamic>>>(
+          statusCode: 200,
+          requestOptions: RequestOptions(path: ''),
+        ),
+      );
+      try {
+        await characterRemoteService.getAllCharacters();
+      } catch (e) {
+        /// Then
+        expect(e, isA<Exception>());
+        verify(() => dioMock.get(AppEndpoints.allCharacters)).called(1);
+        verifyNoMoreInteractions(dioMock);
+      }
+    });
+  });
+
+  group('Testing Characters Remote GetById ', () {
+    final dioMock = DioMock();
+    final characterRemoteService = CharacterRemoteService(dio: dioMock);
+    setUpAll(() => reset(dioMock));
+    test('- Success', () async {
+      /// Given
+      // final characterRemoteService
+
+      /// When
+      when(() => dioMock.get<List<Map<String, dynamic>>>(
+            AppEndpoints.charactersById.replaceAll('{{id}}', '1'),
+          )).thenAnswer(
+        (_) async => Response<List<Map<String, dynamic>>>(
+          data: [
+            {
+              'charId': 1,
+              'name': "Walter White",
+              'birthday': "09-07-1958",
+              'occupation': ["High School Chemistry Teacher", "Meth King Pin"],
+              'img':
+                  "https://images.amcnetworks.com/amc.com/wp-content/uploads/2015/04/cast_bb_700x1000_walter-white-lg.jpg",
+              'status': "Deceased",
+              'appearance': [1, 2, 3, 4, 5],
+              'nickname': "Heisenberg",
+              'portrayed': "Bryan Cranston",
+              'betterCallSaulAppearance': [],
+              'category': '',
+            }
+          ],
+          statusCode: 200,
+          requestOptions: RequestOptions(path: ''),
+        ),
+      );
+
+      final res = await characterRemoteService.characterById(1);
+
+      /// Then
+      expect(res, isA<CharacterModel>());
+
+      verify(() => dioMock.get(
+            AppEndpoints.charactersById.replaceAll('{{id}}', '1'),
+          )).called(1);
+      verifyNoMoreInteractions(dioMock);
+    });
+    test('- Exception', () async {
+      /// Given
+      // final characterRemoteServiceMock
+
+      /// When
+      when(() => dioMock.get<List<Map<String, dynamic>>>(
+            AppEndpoints.charactersById.replaceAll('{{id}}', '1'),
+          )).thenThrow(Exception());
+      try {
+        await characterRemoteService.characterById(1);
+      } catch (e) {
+        /// Then
+        expect(e, isA<Exception>());
+        verify(() => dioMock.get(
+              AppEndpoints.charactersById.replaceAll('{{id}}', '1'),
+            )).called(1);
+        verifyNoMoreInteractions(dioMock);
+      }
+    });
+    test('- No 200', () async {
+      /// Given
+      // final characterRemoteServiceMock
+
+      /// When
+      when(() => dioMock.get<List<Map<String, dynamic>>>(
+            AppEndpoints.charactersById.replaceAll('{{id}}', '1'),
+          )).thenAnswer(
+        (_) async => Response<List<Map<String, dynamic>>>(
+          data: [],
+          statusCode: 0,
+          requestOptions: RequestOptions(path: ''),
+        ),
+      );
+      try {
+        await characterRemoteService.characterById(1);
+      } catch (e) {
+        /// Then
+        expect(e, isA<Exception>());
+        verify(() => dioMock.get(
+              AppEndpoints.charactersById.replaceAll('{{id}}', '1'),
+            )).called(1);
+        verifyNoMoreInteractions(dioMock);
+      }
+    });
+    test('- null data', () async {
+      /// Given
+      // final characterRemoteServiceMock
+
+      /// When
+      when(() => dioMock.get<List<Map<String, dynamic>>>(
+            AppEndpoints.charactersById.replaceAll('{{id}}', '1'),
+          )).thenAnswer(
+        (_) async => Response<List<Map<String, dynamic>>>(
+          statusCode: 200,
+          requestOptions: RequestOptions(path: ''),
+        ),
+      );
+      try {
+        await characterRemoteService.characterById(1);
+      } catch (e) {
+        /// Then
+        expect(e, isA<Exception>());
+        verify(() => dioMock.get(
+              AppEndpoints.charactersById.replaceAll('{{id}}', '1'),
+            )).called(1);
+        verifyNoMoreInteractions(dioMock);
       }
     });
   });
