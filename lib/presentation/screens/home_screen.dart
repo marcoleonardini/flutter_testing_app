9cb9da2d1080d3b72e976a33ddad9ea3369d3299
Marco Leonardini <marco_leonardini@hotmail.com>
2021-07-03 22:03:09 -0400

    Added home screen

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_testing_app/bloc/character_bloc.dart';
 import 'package:flutter_testing_app/repositories/character_repository.dart';
+import 'package:flutter_testing_app/services/models/character_models.dart';
 import 'package:flutter_testing_app/services/remote/character_remote_service.dart';
 
 class CharactersScreen extends StatelessWidget {
@@ -14,15 +15,42 @@ class CharactersScreen extends StatelessWidget {
     final characterRepository =
         CharacterRepositoryImpl(characterRemoteService: characterRemoteService);
     final characterBloc =
-        CharacterBloc(characterRepository: characterRepository);
+        CharacterBloc(characterRepository: characterRepository)
+          ..allCharacters();
 
     return Scaffold(
       body: Container(
         constraints: const BoxConstraints.expand(),
         child: StreamBuilder(
           stream: characterBloc.subject,
-          builder: (context, snapshot) {
-            return Container();
+          builder: (context, AsyncSnapshot<CharactersResponse> snapshot) {
+            if (!snapshot.hasData) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            if (snapshot.hasError) {
+              return const Center(
+                child: Icon(
+                  Icons.close,
+                  color: Colors.red,
+                ),
+              );
+            }
+            if (snapshot.data!.results == null) {
+              return const Center(
+                child: Icon(
+                  Icons.close,
+                  color: Colors.red,
+                ),
+              );
+            }
+            return ListView.builder(
+              itemCount: snapshot.data!.results!.length,
+              itemBuilder: (context, index) {
+                return Image.network(snapshot.data!.results![index].img);
+              },
+            );
           },
         ),
       ),
